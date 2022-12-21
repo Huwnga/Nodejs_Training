aa6b42bdaf53e214d0f1aa29ead5ecbe23477a31
Huwnga <hungbungbu2003@gmail.com>
2022-12-21 18:39:11 +0700

    install fe enviroment again

--- FE/app.js
+++ FE/app.js
@@ -1,7 +1,30 @@
-const path = require("path");
-const bodyParser = require("body-parser");
-const express = require("express");
+// Call library
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.listen(8080);
+// Call routes
+const signin_up_outRoutes = require('./routes/signin_up_out');
+const adminRoutes = require('./routes/admin');
+const teacherRoutes = require('./routes/teacher');
+const studentRoutes = require('./routes/student');
+
+// set views
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+
+//  Set library
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Use routes
+app.use('/auth', signin_up_outRoutes);
+app.use('/admin', adminRoutes);
+app.use('/teacher', teacherRoutes);
+app.use(studentRoutes);
+app.use(errorController.get404);
+
+app.listen(3000);
